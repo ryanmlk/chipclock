fe53462054255fdc6613703a3fd783e50b587c2b
ryanmlk <ryanmoses99@gmail.com>
2026-02-26 19:04:19 -0500

    Updating function call from test to parser

--- chip_clock/api/test_ts.ts
+++
@@ -1,5 +0,0 @@
-export async function GET(request: Request) {
-    const response = await fetch('https://api.vercel.app/products');
-    const products = await response.json();
-    return Response.json(products);
-}
